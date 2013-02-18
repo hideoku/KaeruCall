57538b8cbd0fad9e1a1d88f74a701f0b29811d05
hideoku <hideoku487@gmail.com>
2013-02-18 23:47:51 +0900

    電話をかけられるようにする。

--- src/spring/hope/KaeruCall/MyActivity.java
+++ src/spring/hope/KaeruCall/MyActivity.java
@@ -1,15 +1,33 @@
 package spring.hope.KaeruCall;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MyActivity extends Activity
-{
+public class MyActivity extends Activity {
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        setTitle(R.string.title_main);
+
+        Button buttonCall = (Button) this.findViewById(R.id.buttonCall);
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+//                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:1234567890"));
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
